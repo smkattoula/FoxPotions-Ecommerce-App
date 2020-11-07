bd7fca4243ed0d062468826ed79d7f28793787be
Shaker Kattoula <smkattoula@gmail.com>
2020-11-06 19:32:01 -0800

    custom error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,15 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
