fbe47502dffaf27d60551315f86fa97d09d751d6
Shaker Kattoula <smkattoula@gmail.com>
2020-11-30 16:50:11 -0800

    create register user route and setup password encryption middleware with mongoose

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,4 +43,36 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+// Description: Register a new user
+// Route: POST /api/users/
+// Access: Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data");
+  }
+});
+
+export { authUser, registerUser, getUserProfile };
