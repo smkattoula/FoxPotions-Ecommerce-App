abb2d9d13de90f462e5ee08d5281aa0669a43e45
Shaker Kattoula <smkattoula@gmail.com>
2020-12-03 13:22:04 -0800

    create UPDATE profile api route in the backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -75,4 +75,31 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile };
+// Description: Update a user profile
+// Route: PUT /api/users/profile
+// Access: Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile };
