f5657508b1f2b6caae114fb80e5adab98436f589
Shaker Kattoula <smkattoula@gmail.com>
2021-01-21 17:17:13 -0800

    show orders on profile screen

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -79,4 +79,12 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid };
+// Description: Get logged in user orders
+// Route: GET /api/orders/myorders
+// Access: Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
